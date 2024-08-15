c98f89b21a2f84820fd5cf223a25758a561db6c4
MRSS02 <mateusyasd@hotmail.com>
2024-08-14 23:59:23 -0300

    AAAAAAAA

--- E3/src/token.h
+++ E3/src/token.h
@@ -1,13 +1,30 @@
 /* token.h */
 
 typedef enum {
-        ID = 256, 
-	NUM, 
-        KEY,
-        SYM,  
-        SYMC, 
-        CHAR,
-	STR,
-	ERROR
+    ID = 256, // Identificador
+    UNKNOWN_1,
+    UNKNOWN_2,
+    BOOLEAN, 
+    CHAR,
+    ELSE,
+    FALSE,
+    FOR,
+    FUNCTION,
+    IF,
+    INTEGER,
+    PRINT,
+    RETURN,
+    STRING,
+    TRUE,
+    VOID,
+    WHILE,
+    SMALL_EQ,
+    GREAT_EQ,
+    EQ,
+    NOT_EQ,
+    AND,
+    OR,
+
+    
 } token_t; 
 
