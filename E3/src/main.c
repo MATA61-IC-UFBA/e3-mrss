70d2a678134c638b09b729a9a41fc414c50eaa07
MRSS02 <mateusyasd@hotmail.com>
2024-09-06 22:05:04 -0300

    correções

--- E3/src/main.c
+++ E3/src/main.c
@@ -42,29 +42,98 @@ int main(int argc, char **argv) {
 				// printf("(%d,NUM,\"%s\")\n", yylineno, yytext);
 				fprintf(yyout, "(%d,NUM,\"%s\")\n", yylineno, yytext);
 			    break;
-			case STR:
+			case ARRAY:
 				// printf("(%d,STR,\"%s\")\n", yylineno, yytext);
-				fprintf(yyout, "(%d,STR,\"%s\")\n", yylineno, yytext);
+				fprintf(yyout, "(%d,ARRAY,\"%s\")\n", yylineno, yytext);
+			    break;
+			case BOOLEAN:
+				// printf("(%d,CHAR,\"%s\")\n", yylineno, yytext);
+				fprintf(yyout, "(%d,BOOLEAN,\"%s\")\n", yylineno, yytext);
 			    break;
 			case CHAR:
-				// printf("(%d,CHAR,\"%s\")\n", yylineno, yytext);
+				// printf("(%d,KEY,\"%s\")\n", yylineno, yytext);
 				fprintf(yyout, "(%d,CHAR,\"%s\")\n", yylineno, yytext);
 			    break;
-			case KEY:
-				// printf("(%d,KEY,\"%s\")\n", yylineno, yytext);
-				fprintf(yyout, "(%d,KEY,\"%s\")\n", yylineno, yytext);
-			    break;
-			case SYM:
+			case ELSE:
 				// printf("(%d,SYM,\"%s\")\n", yylineno, yytext);
-				fprintf(yyout, "(%d,SYM,\"%s\")\n", yylineno, yytext);
+				fprintf(yyout, "(%d,ELSE,\"%s\")\n", yylineno, yytext);
 			    break;
-			case SYMC:
+			case FALSE:
 				// printf("(%d,SYMC,\"%s\")\n", yylineno, yytext);
-				fprintf(yyout, "(%d,SYMC,\"%s\")\n", yylineno, yytext);
+				fprintf(yyout, "(%d,FALSE,\"%s\")\n", yylineno, yytext);
+			    break;
+			case FOR:
+				// printf("(%d,ERROR,\"%s\")\n", yylineno, yytext);
+				fprintf(yyout, "(%d,FOR,\"%s\")\n", yylineno, yytext);
+                            break;
+                        case FUNCTION:
+				// printf("(%d,ID,\"%s\")\n", yylineno, yytext);
+				fprintf(yyout, "(%d,FUNCTION,\"%s\")\n", yylineno, yytext);
+			    break;
+			case IF:
+				// printf("(%d,NUM,\"%s\")\n", yylineno, yytext);
+				fprintf(yyout, "(%d,IF,\"%s\")\n", yylineno, yytext);
+			    break;
+			case INTEGER:
+				// printf("(%d,STR,\"%s\")\n", yylineno, yytext);
+				fprintf(yyout, "(%d,INTEGER,\"%s\")\n", yylineno, yytext);
+			    break;
+			case PRINT:
+				// printf("(%d,CHAR,\"%s\")\n", yylineno, yytext);
+				fprintf(yyout, "(%d,PRINT,\"%s\")\n", yylineno, yytext);
+			    break;
+			case RETURN:
+				// printf("(%d,KEY,\"%s\")\n", yylineno, yytext);
+				fprintf(yyout, "(%d,RETURN,\"%s\")\n", yylineno, yytext);
+			    break;
+			case STRING:
+				// printf("(%d,SYM,\"%s\")\n", yylineno, yytext);
+				fprintf(yyout, "(%d,STRING,\"%s\")\n", yylineno, yytext);
+			    break;
+			case TRUE:
+				// printf("(%d,SYMC,\"%s\")\n", yylineno, yytext);
+				fprintf(yyout, "(%d,TRUE,\"%s\")\n", yylineno, yytext);
+			    break;
+			case VOID:
+				// printf("(%d,ERROR,\"%s\")\n", yylineno, yytext);
+				fprintf(yyout, "(%d,VOID,\"%s\")\n", yylineno, yytext);
+                            break;
+                        case WHILE:
+				// printf("(%d,ERROR,\"%s\")\n", yylineno, yytext);
+				fprintf(yyout, "(%d,WHILE,\"%s\")\n", yylineno, yytext);
+                            break;
+                        case LTE:
+				// printf("(%d,ID,\"%s\")\n", yylineno, yytext);
+				fprintf(yyout, "(%d,LTE,\"%s\")\n", yylineno, yytext);
+			    break;
+			case GTE:
+				// printf("(%d,NUM,\"%s\")\n", yylineno, yytext);
+				fprintf(yyout, "(%d,GTE,\"%s\")\n", yylineno, yytext);
+			    break;
+			case EQU:
+				// printf("(%d,STR,\"%s\")\n", yylineno, yytext);
+				fprintf(yyout, "(%d,EQU,\"%s\")\n", yylineno, yytext);
+			    break;
+			case NEQ:
+				// printf("(%d,CHAR,\"%s\")\n", yylineno, yytext);
+				fprintf(yyout, "(%d,NEQ,\"%s\")\n", yylineno, yytext);
+			    break;
+			case AND:
+				// printf("(%d,KEY,\"%s\")\n", yylineno, yytext);
+				fprintf(yyout, "(%d,AND,\"%s\")\n", yylineno, yytext);
+			    break;
+			case OR:
+				// printf("(%d,SYM,\"%s\")\n", yylineno, yytext);
+				fprintf(yyout, "(%d,OR,\"%s\")\n", yylineno, yytext);
+			    break;
+			case STR:
+				// printf("(%d,SYMC,\"%s\")\n", yylineno, yytext);
+				fprintf(yyout, "(%d,STR,\"%s\")\n", yylineno, yytext);
 			    break;
 			case ERROR:
 				// printf("(%d,ERROR,\"%s\")\n", yylineno, yytext);
 				fprintf(yyout, "(%d,ERROR,\"%s\")\n", yylineno, yytext);
+                            break;
 
 		}
 	}
